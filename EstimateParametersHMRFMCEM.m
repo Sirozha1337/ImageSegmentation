8f4ab6a3492f2c992b3cf0f133f852fb00685df1
Sergey Chernyaev <schernyae@gmail.com>
2019-04-24 13:33:40 +0300

    [FEATURE] Логирование ошибок в функциях оценки параметров

--- EstimateParametersHMRFMCEM.m
+++ EstimateParametersHMRFMCEM.m
@@ -29,7 +29,12 @@ for l=1:k
     eqn = @(x) besseli(p/2, x)/besseli(p/2-1,x) * N / M - sqrt(sum(R .^ 2)) / M;
     initval = kappa(l);
     if isnan(eqn(initval))
+        fprintf('WARNING: function is NaN in initial point\n')
         initval = 10;
     end
     kappa(l) = fzero(eqn, initval);
+    if kappa(l) < 0 || isnan(kappa(l))
+        kappa(l) = abs(kappa(l));
+        fprintf('WARNING: Lower than zero solution was found\n')
+    end
 end
