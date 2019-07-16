c1d98d6734cdf69406dd5f8d2337f5c492cdc272
Sergey Chernyaev <schernyae@gmail.com>
2019-07-16 11:24:06 +0300

    Return all mus and kappas for plotting

--- Grab_Cut.m
+++ Grab_Cut.m
@@ -18,23 +18,27 @@
 %   beta: оцененное значение beta
 %   mus: оцененное значение mus
 %   kappas: оцененное значение kappas
-function [sample, mus, kappas] = Grab_Cut(data, segment, k, lambda, sigma, mus, kappas, map_iter, max_iter, neighbours_count)
+function [sample, mus, kappas, all_mus, all_kappas] = Grab_Cut(data, segment, k, lambda, sigma, mus, kappas, map_iter, max_iter, neighbours_count)
 
 dim = size(segment);
 p = size(data, 2);
+all_mus = zeros([max_iter, size(mus)]);
+all_kappas = zeros([max_iter, size(kappas)]);
 
 for i=1:max_iter
     fprintf('\t GrabCut Iteration: %d of %d\n',i,max_iter);
     % подстраиваем параметры
     [mus, kappas] = EstimateParametersGrabCut(data, segment, k, p, mus, kappas);
+    all_mus(i, :, :) = mus;
+    all_kappas(i, :) = kappas;
     % считаем вероятности
     [~, logprobs] = CalculateLikelihoodProbabilities(data, k, kappas, mus);
     % считаем Bpq
-    Neighbours = GetNeighbours(dim, neighbours_count);
-    Bpq = lambda * exp(sum((repmat(data, neighbours_count, 1)-data(Neighbours, :)).^2, 2)./(2*sigma^2));
-    Bpq = reshape(Bpq, [ prod(dim), neighbours_count ]);
+    %Neighbours = GetNeighbours(dim, neighbours_count);
+    %Bpq = lambda * exp(sum((repmat(data, neighbours_count, 1)-data(Neighbours, :)).^2, 2)./(2*sigma^2));
+    %Bpq = reshape(Bpq, [ prod(dim), neighbours_count ]);
     % находим MAP
-    [segment, ~] = MRF_MAP_GraphCutABSwap(segment, logprobs, Bpq, k, map_iter, neighbours_count);
+    [segment, ~] = MRF_MAP_GraphCutABSwap(segment, logprobs, 2, k, map_iter, neighbours_count);
 end
 
 sample = reshape(segment, dim);
