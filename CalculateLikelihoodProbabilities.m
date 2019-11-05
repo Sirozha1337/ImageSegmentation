6eeff59a4aee7a2f8f6a84a3cf4c678f54ef35bd
Sergey Chernyaev <schernyae@gmail.com>
2019-11-05 22:49:11 +0300

    [FIX] Add ability to mask voxels when calculating likelihood, fix NaNs in likelihood

--- CalculateLikelihoodProbabilities.m
+++ CalculateLikelihoodProbabilities.m
@@ -4,15 +4,20 @@
 % k: number of classes
 % kappas: vMF distribution parameters, Kx1
 % mus: vMF ditribution parameters, KxP
+% mask: 0-1 matrix size of data
 %---output--------------------------------------------------------
 % probs: likelihood probabilities, LxN
 % logprobs: loglikelihood probabilities, LxN
-function [probs,logprobs] = CalculateLikelihoodProbabilities(data, k, kappas, mus)
+function [probs,logprobs] = CalculateLikelihoodProbabilities(data, k, kappas, mus, mask)
+
+if nargin < 5
+  mask = ones(size(data, 1), 1);
+end
 
 probs = zeros([k, size(data, 1)]);
 logprobs = zeros([k, size(data, 1)]);
 
 for i=1:k
-    logprobs(i, :) = -kappas(i) * mus(i, :) * data' - log(C(size(data,2), kappas(i)));
-    probs(i, :) = C(size(data,2), kappas(i)) * min(exp(kappas(i) * mus(i, :) * data'), 10^9);
+    logprobs(i, mask~=0) = -kappas(i) * mus(i, :) * data(mask~=0, :)' - log(C(size(data,2), kappas(i)));
+    probs(i, mask~=0) = C(size(data,2), kappas(i)) * min(exp(kappas(i) * mus(i, :) * data(mask~=0, :)'), 10^100);
 end
