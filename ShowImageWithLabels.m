fd1d068fd106c58d9be7b142bdd62bba3f01689f
Sergey Chernyaev <schernyae@gmail.com>
2019-11-05 22:59:27 +0300

    [FEATURE] Function for displaying fmri image slices overlayed with labels

---
+++ ShowImageWithLabels.m
@@ -0,0 +1,17 @@
+%%  Shows slice of an image overlayed with labels
+%---input---------------------------------------------------------
+%
+%
+%---output--------------------------------------------------------
+%
+function [] = ShowImageWithLabels(image, labels, direction, slice_num)
+C = repmat({':'},1,ndims(image));
+C(direction) = {slice_num};
+slice = image(C{:});
+slice = mat2gray(slice);
+
+C = repmat({':'},1,ndims(labels));
+C(direction) = {slice_num};
+labels_slice = labels(C{:});
+overlayed_img = labeloverlay(slice,labels_slice);
+imshow(overlayed_img);
