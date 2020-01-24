e3bbc0057906d8297a6984585da3eff46e7c6e97
Sergey Chernyaev <schernyae@gmail.com>
2020-01-24 20:08:14 +0300

    Add function to visualise segmented fmri image

---
+++ ShowMultipleSlicesWithLabels.m
@@ -0,0 +1,21 @@
+%%  Shows multiple slices with labels
+%---input---------------------------------------------------------
+%
+%
+%---output--------------------------------------------------------
+%
+function [] = ShowMultipleSlicesWithLabels(image, labels, directions, slice_nums, titles)
+
+len = numel(directions);
+
+figure;  
+for i=1:len
+    direction = directions(i);
+    slice_num = slice_nums(i);
+    subplot(len,1,i)
+	ShowImageWithLabels(image, labels, direction, slice_num); 
+end
+currentFigure = gcf;
+for i=1:len
+    title(currentFigure.Children(len-i+1), titles(i));
+end
