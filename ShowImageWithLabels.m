696c74670a60e05d3251ac435b12964cbf4765ed
Sergey Chernyaev <schernyae@gmail.com>
2020-01-24 20:05:54 +0300

    Fix show image with labels to work with multidimensional images

--- ShowImageWithLabels.m
+++ ShowImageWithLabels.m
@@ -8,10 +8,10 @@ function [] = ShowImageWithLabels(image, labels, direction, slice_num)
 C = repmat({':'},1,ndims(image));
 C(direction) = {slice_num};
 slice = image(C{:});
-slice = mat2gray(slice);
+slice = squeeze(mat2gray(slice));
 
 C = repmat({':'},1,ndims(labels));
 C(direction) = {slice_num};
-labels_slice = labels(C{:});
+labels_slice = squeeze(labels(C{:}));
 overlayed_img = labeloverlay(slice,labels_slice);
 imshow(overlayed_img);
